295e479439cd264da86cd0a1efc9b962cf4b0f38
Sanjay Patni <sanjaypatni@Sanjays-MacBook-Pro.local>
2022-10-27 05:15:44 -0700

    updates

---
+++ chapter5/message/src/main/java/com/rest/model/Message.java
@@ -0,0 +1,40 @@
+package com.rest.model;
+public class Message {
+ private int id;
+ private String message;
+ private String from;
+ private String to;
+ private String creationDate;
+
+ public String getMessage() {
+        return message;
+ }
+ public void setMessage(String message) {
+     this.message = message;
+ }
+ public String getFrom() {
+        return from;
+ }
+ public void setFrom(String from) {
+     this.from = from;
+ }
+ public String getTo() {
+        return to;
+ }
+ public void setTo(String to) {
+     this.to = to;
+ }
+ public String getCreationDate() {
+        return creationDate;
+ }
+ public void setCreationDate(String creationDate) {
+     this.creationDate = creationDate;
+ }
+ public int getId() {
+        return id;
+ }
+ public void setId(int id) {
+     this.id = id;
+ }
+}
+
